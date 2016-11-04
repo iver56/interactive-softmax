c251106d2d9cb0390a885addc8c15065524dc477
iver56 <iver56@hotmail.com>
2016-11-04 13:33:10 +0100

    Add radio buttons for range

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,35 @@
 function formatValue(value) {
-  return parseFloat(Math.round(value * 100) / 100).toFixed(2);
+  return parseFloat(value).toPrecision(2);
 }
 
 function update() {
+  var range = $('input[name=range]:checked', 'form').val();
+  range = eval(range);
+
   var values = [];
+
+  var softmaxDenominator = 0;
 
   $('.inputs input[type=range]').each(function(index) {
     var $slider = $(this);
     var $label = $("label[for='" + $slider.attr('id') + "']");
     var value = $slider.val();
+    value = range[0] + (range[1] - range[0]) * value;
     values.push(value);
+    softmaxDenominator += Math.exp(value);
     $label.text(formatValue(value));
   });
-
-  var softmaxDenominator = 0;
-  for (value of values) {
-    softmaxDenominator += Math.exp(value);
-  }
 
   $('.outputs input[type=range]').each(function(index) {
     var $slider = $(this);
     var $label = $("label[for='" + $slider.attr('id') + "']");
     var softmaxValue = Math.exp(values[index]) / softmaxDenominator;
-    var value = $slider.val(softmaxValue);
     $label.text(formatValue(softmaxValue));
+    $slider.val(softmaxValue);
   });
 }
 
-$("input[type=range]").on("input change", function() {
+$("input").on("input change", function() {
   update();
 });
 
